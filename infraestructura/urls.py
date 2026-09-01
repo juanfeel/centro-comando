f98b28611e7bedabb7e0712daf8af1fa2baebd06
juanfel <juanfel2006@gmail.com>
2026-09-01 12:24:56 -0500

    creacion detalle servidor

--- infraestructura/urls.py
+++ infraestructura/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import lista_servidores
+from .views import lista_servidores, detalle_servidor
 
 urlpatterns = [
     path('', lista_servidores, name='lista_servidores'),
+    path('servidor/<int:pk>/',detalle_servidor, name = 'detalle_servidor')
 ]
--- infraestructura/views.py
+++ infraestructura/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import NodoServidor
+
+def detalle_servidor(request, pk):
+	nodo = get_object_or_404(NodoServidor, pk=pk)
+	return render(request, 'infraestructura/detalle.html', {'nodo':nodo})
 
 def lista_servidores(request):
 	servidores = NodoServidor.objects.all()
