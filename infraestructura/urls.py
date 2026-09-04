b94e259869fe2871e698cc615422f325dcb7fd4f
juanfel <juanfel2006@gmail.com>
2026-09-04 17:55:57 -0500

    creacion CRUD completo, falta comprobar funcionalidad

--- infraestructura/urls.py
+++ infraestructura/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import lista_servidores, detalle_servidor
+from .views import lista_servidores, detalle_servidor, crear_servidor, editar_servidor, eliminar_servidor
 
 urlpatterns = [
     path('', lista_servidores, name='lista_servidores'),
-    path('servidor/<int:pk>/',detalle_servidor, name = 'detalle_servidor')
+    path('servidor/<int:pk>/',detalle_servidor, name = 'detalle_servidor'),
+    path('servidor/nuevo/', crear_servidor, name='crear_servidor'),
+    path('servidor/<int:pk>/editar/', editar_servidor, name='editar_servidor'),
+    path('servidor/<int:pk>/eliminar/', eliminar_servidor, name='eliminar_servidor'),
 ]
